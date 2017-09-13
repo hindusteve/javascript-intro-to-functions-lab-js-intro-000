81e2d3a7e02e8498ea7b841b6195e12368c95a84
Abhijit <abhijit.khanna@live.com>
2017-09-13 20:51:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ function logShout(string) {
 function logWhisper(string) {
   return console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+
+  var uppercase = string.toUpperCase();
+  var lowercase = string.toLowerCase();
+  
+    if string === "I love you, Grandma." {
+      return "I love you, too."
+    } else if string.toUpperCase() === uppercase {
+      return "YES INDEED!"
+    } else if string.toLowerCase() === lowercase {
+      return "I can't hear you!"
+    }
+}
