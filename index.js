e0f558cf6fe1638b813f5c1d018db40ff7c971c4
Abhijit <abhijit.khanna@live.com>
2017-09-13 20:43:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 
 function shout(string) {
   return string.toUpperCase();
+}
 
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  return console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase());
 }
